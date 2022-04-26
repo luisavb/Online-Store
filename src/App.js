5c3a883a2a04dcfd1f2a291ffd6efd96e27368a7
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-04-26 16:02:39 -0300

    Conclui requisito 1, consolelog para funções do api.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  async componentDidMount() {
+    await this.teste();
+  }
+
+  teste = async () => {
+    getCategories().then((categories) => { console.log(categories); });
+    getProductsFromCategoryAndQuery().then((categories) => { console.log(categories); });
+  };
+
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
 }
 
 export default App;
