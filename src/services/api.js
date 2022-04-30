b33a73d9b7c843797fb45240c7b81d6f5c2e34a0
Felipe <felmartins1985@gmail.com>
2022-04-30 09:38:55 -0300

    requisito 1 comentado

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,18 @@
+// função para buscar a categoria do produto
 export async function getCategories() {
   // Implemente aqui
   const request = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const requestJson = await request.json();
   return requestJson;
 }
-
+// função para buscar por meio da consulta as categorias
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const requestJson = await request.json();
   return requestJson;
 }
-
+// função para buscar os detalhes do produto
 export async function getProductDetail(produtoId) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   const request = await fetch(`https://api.mercadolibre.com/items/${produtoId}`);
