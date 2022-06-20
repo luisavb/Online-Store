1ed8e2bc4b916b35de7cd04be9a7b5f7426fb945
Luísa Vilas Boas dos Santos <luisa13santos@outlook.com>
2022-06-19 23:50:55 -0300

    Updates

---
+++ precache-manifest.5488062bc2a0264906d4df95ad0f8135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc744b14343cfc10266a3f066275710",
+    "url": "/Online-Store/index.html"
+  },
+  {
+    "revision": "bbfd4a4479d8eabc2695",
+    "url": "/Online-Store/static/css/main.cd66b8de.chunk.css"
+  },
+  {
+    "revision": "4d41d1999f0f3ca4f912",
+    "url": "/Online-Store/static/js/2.3c9714be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Online-Store/static/js/2.3c9714be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbfd4a4479d8eabc2695",
+    "url": "/Online-Store/static/js/main.0dbb13db.chunk.js"
+  },
+  {
+    "revision": "b692c4d9c5923666b4ed",
+    "url": "/Online-Store/static/js/runtime-main.b7962b94.js"
+  }
+]);
